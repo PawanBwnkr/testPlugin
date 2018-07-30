e39f81a9842b37880db2d9b4294460f1b866b0d6
Pawan Bawankar <pawan.bawankar@datami.com>
2018-07-30 16:13:00 +0530

    code changed

---
+++ smisdk-ios-plugin/RNDatamiEventManager.h
@@ -0,0 +1,18 @@
+//
+//  RNDatamiEventManager.h
+//  RNDatamiSdk
+//
+//  Created by Sonali Sagar on 30/07/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#if __has_include("RCTEventEmitter.h")
+#import "RCTEventEmitter.h"
+#else
+#import <React/RCTEventEmitter.h>
+#endif
+
+@interface RNDatamiEventManager : RCTEventEmitter 
+
+@end
